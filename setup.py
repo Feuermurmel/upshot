ff65d057250bb5ab49eb7f58d270a789cce446de
Michael Schwarz <michi.schwarz@gmail.com>
2016-05-09 00:18:49 +0200

    Explicitly state packages.

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ import setuptools
 setuptools.setup(
 	name = 'Upshot',
 	version = '0.1',
-	packages = setuptools.find_packages(),
-	package_data = { '': ['*.*'] },
+	packages = ['upshot'],
+	package_data = { 'upshot': ['*.*'] },
 	install_requires = ['setuptools', 'markdown', 'pygments', 'py-gfm', 'python-markdown-math', 'pyexecjs', 'pystache'],
 	entry_points = dict(
 		console_scripts = [
